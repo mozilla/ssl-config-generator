b273914ae483723e6adce88f3778408c850b598e
April King <april@mozilla.com>
2019-05-30 12:04:01 -0500

    Remove frame-ancestors from CSP

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   author: "April King",
-  contentSecurityPolicy: "default-src 'none'; base-uri 'none'; frame-ancestors https:; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'",
+  contentSecurityPolicy: "default-src 'none'; base-uri 'none'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'",
   localContentSecurityPolicy: "default-src * 'unsafe-inline'",  // supports autoreload
   validHashKeys: ["server", "server-version", "openssl-version", "config", "hsts", "ocsp"],
   title: "SSL Configuration Generator",
