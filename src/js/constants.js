1d56492f605122bf9f040967bd28f930c144ec22
April King <april@mozilla.com>
2019-07-17 15:46:31 -0500

    Add GA to CSP

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   author: "April King",
-  contentSecurityPolicy: "default-src 'none'; base-uri 'none'; font-src 'self'; img-src 'self'; script-src 'self' https://www.googletagmanager.com/gtag/js; style-src 'self'",
+  contentSecurityPolicy: "default-src 'none'; base-uri 'none'; font-src 'self'; img-src 'self'; script-src 'self' https://www.google-analytics.com/analytics.js https://www.googletagmanager.com/gtag/js; style-src 'self'",
   description: "An easy-to-use secure configuration generator for web, database, and mail software",
   header: "SSL Configuration Generator",
   localContentSecurityPolicy: "default-src * 'unsafe-inline'",  // supports autoreload
