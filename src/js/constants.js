4124b4f88d28e0fc6567bb581b23f3d097e733fd
April King <april@mozilla.com>
2020-09-22 10:43:54 -0500

    flesh out description a bit

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   author: "April King",
   contentSecurityPolicy: "default-src 'none'; base-uri 'none'; connect-src https://www.google-analytics.com; font-src 'self'; img-src 'self' https://www.google-analytics.com; script-src 'self' https://www.google-analytics.com/analytics.js https://www.googletagmanager.com/gtag/js; style-src 'self'",
-  description: "An easy-to-use secure configuration generator for web, database, and mail software",
+  description: "An easy-to-use secure configuration generator for web, database, and mail software. Simply select the software you are using and receive a configuration file that is both safe and compatible.",
   header: "SSL Configuration Generator",
   localContentSecurityPolicy: "default-src * 'unsafe-inline'",  // supports autoreload
   mobileHeader: "SSL Config Generator",
