0ce8673f69a08dddad3579c455d950a90a2d7bd8
Andrew Wilson <87784967+aw-engineer@users.noreply.github.com>
2025-09-07 19:20:54 +0000

    GH-332 updates syntax for dovecot >= 2.4.0
    reformatted dovecot.js with
    $ npx standard src/js/helpers/dovecot.js --fix

--- src/js/helpers/dovecot.js
+++ src/js/helpers/dovecot.js
@@ -1,40 +1,50 @@
-import minver from './minver.js';
+import minver from './minver.js'
 
 export default (form, output) => {
- var conf =
-      '# '+output.header+'\n'+
-      '# '+output.link+'\n'+
-      'ssl = required\n'+
-      '\n'+
-      'ssl_cert = </path/to/signed_cert_plus_intermediates\n'+
-      'ssl_key = </path/to/private_key\n';
+  let conf =
+      '# ' + output.header + '\n' +
+      '# ' + output.link + '\n\n' +
+      'ssl = required\n'
 
- if (output.usesDhe) {
+  if (minver('2.4.0', form.serverVersion)) {
     conf +=
-      '\n'+
-      (minver("2.3.0", form.serverVersion)
-        ?
-      '# '+output.dhCommand+' > /path/to/dhparam\n'+
+      'ssl_server_cert_file = /path/to/signed_cert_plus_intermediates\n' +
+      'ssl_server_key_file = /path/to/private_key\n'
+  } else {
+    conf +=
+      'ssl_cert = </path/to/signed_cert_plus_intermediates\n' +
+      'ssl_key = </path/to/private_key\n'
+
+    if (output.usesDhe) {
+      conf +=
+      '\n' +
+      (minver('2.3.0', form.serverVersion)
+        ? '# ' + output.dhCommand + ' > /path/to/dhparam\n' +
       'ssl_dh = </path/to/dhparam\n'
-        :
-      'ssl_dh_parameters_length = '+output.dhParamSize+'\n');
- }
+        : 'ssl_dh_parameters_length = ' + output.dhParamSize + '\n')
+    }
+  }
 
+  conf += `\n# ${form.config} configuration\n`
+
+  if (minver('2.3.0', form.serverVersion)) {
+    conf += 'ssl_min_protocol = ' + output.protocols[0] + '\n'
+  } else {
+    conf += 'ssl_protocols = ' + output.protocols.join(' ') + '\n'
+  }
+
+  if (minver('2.4.0', form.serverVersion)) {
+    conf += 'ssl_server_prefer_ciphers = ' + (output.serverPreferredOrder ? 'server' : 'client') + '\n'
+  } else {
+    conf += 'ssl_prefer_server_ciphers = ' + (output.serverPreferredOrder ? 'yes' : 'no') + '\n'
+  }
+
+  conf += 'ssl_curve_list = ' + output.tlsCurves.join(':') + '\n'
+
+  if (output.ciphers.length) {
     conf +=
-      '\n'+
-      '# '+form.config+' configuration\n'+
-      (minver("2.3.0", form.serverVersion)
-        ?
-      'ssl_min_protocol = '+output.protocols[0]+'\n'
-        :
-      'ssl_protocols = '+output.protocols.join(' ')+'\n')+
-      'ssl_prefer_server_ciphers = '+(output.serverPreferredOrder ? 'yes' : 'no')+'\n'+
-      'ssl_curve_list = '+output.tlsCurves.join(':')+'\n';
+      'ssl_cipher_list = ' + output.ciphers.join(':') + '\n'
+  }
 
- if (output.ciphers.length) {
-    conf +=
-      'ssl_cipher_list = '+output.ciphers.join(':')+'\n';
- }
-
-  return conf;
-};
+  return conf
+}
